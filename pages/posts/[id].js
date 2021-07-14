dc0b283b255d15a2611443fcf59c031107d7fd40
Eugene Whatley <epwhatley@gmail.com>
2021-07-13 22:01:39 -0400

    Add styling to blog post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import Date from '../../components/date'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id)
@@ -25,13 +27,13 @@ export default function Post({ postData }) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            {postData.title}
-            <br/>
-            {postData.id}
-            <br/>
-            {postData.date}
-            <br/>
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}}/>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
