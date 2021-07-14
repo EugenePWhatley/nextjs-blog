c29553a1caa8a2d9fc5a5b6cedd203bcc88da594
Eugene Whatley <epwhatley@gmail.com>
2021-07-13 23:34:37 -0400

    Create custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+import utilStyles from "../styles/utils.module.css"
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h1 className={utilStyles.headingLg}>404 - Page Not Found</h1>
+        </Layout>
+    )
+}
