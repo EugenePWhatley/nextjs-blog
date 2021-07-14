0a3212bc223077154aa5732b37904d77caa4d400
Eugene Whatley <epwhatley@gmail.com>
2021-07-13 21:53:33 -0400

    Render blog content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,9 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
@@ -21,11 +22,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br/>
             {postData.id}
             <br/>
             {postData.date}
+            <br/>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}}/>
         </Layout>
     )
 }
